bdc0f8ce6261692ee3f4ae9121e288ef9535b79e
mtrop <mtropiano@gmail.com>
2019-08-25 15:03:38 -0400

    Changed some build stuff.

--- shell/compile/app-name.cmd
+++ shell/compile/app-name.cmd
@@ -1,9 +1,9 @@
 @echo off
-REM =========================================================================
+
+REM ============================[ VARIABLES ]================================
 SETLOCAL
 SET JAVAEXE=
 SET JAVAOPTS={{JAVA_OPTIONS}}
-SET JAVACLASSPATH={{COMPILE_CLASSPATH}}
 SET JAVAJAR={{JAR_NAME}}
 SET MAINCLASS={{MAIN_CLASSNAME}}
 REM =========================================================================
@@ -11,12 +11,16 @@ REM =========================================================================
 where java > nul
 if ERRORLEVEL == 0 (SET JAVAEXE=java)
 if not %JAVAEXE%=="" goto _calljava
+
 if not %JAVA_HOME%=="" (SET JAVAEXE=%JAVA_HOME%\bin)
 if not %JAVAEXE%=="" goto _calljava
+
 if not %JRE_HOME%=="" (SET JAVAEXE=%JRE_HOME%\bin)
 if not %JAVAEXE%=="" goto _calljava
+
 if not %JDK_HOME%=="" (SET JAVAEXE=%JDK_HOME%\bin)
 if not %JAVAEXE%=="" goto _calljava
+
 goto _end
 
 :_calljava
---
+++ shell/compile/app-name.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CMD_READLINK="readlink -f"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	function realpath {
+    	[ "." = "${1}" ] && n=${PWD} || n=${1}; while nn=$( readlink -n "$n" ); do n=$nn; done; echo "$n"
+	}
+	CMD_READLINK="realpath"
+fi
+SCRIPTDIR="$(cd "$(dirname $($CMD_READLINK "$0"))"; pwd)"
+
+JAVAOPTS={{JAVA_OPTIONS}}
+JAVAJAR={{JAR_NAME}}
+MAINCLASS={{MAIN_CLASSNAME}}
+
+java -cp "$SCRIPTDIR\$JAVAJAR" $JAVAOPTS $MAINCLASS %*
