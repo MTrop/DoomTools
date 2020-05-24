fa2d66988dda2f7b716f3e99c06897029ce67bb6
mtrop <mtropiano@gmail.com>
2020-05-23 21:50:07 -0400

    Fixed scripts

--- shell/jar/app-name.cmd
+++ shell/jar/app-name.cmd
@@ -8,20 +8,27 @@ SET JAVAJAR={{JAR_NAME}}
 SET MAINCLASS={{MAIN_CLASSNAME}}
 REM =========================================================================
 
+REM ======= Java Scan =======
+
+if exist "%~dp0\jre\bin\java.exe" SET JAVAEXE=%~dp0\jre\bin\java.exe
+if not "%JAVAEXE%"=="" goto _calljava
+
 where java > nul
-if ERRORLEVEL == 0 (SET JAVAEXE=java)
-if not %JAVAEXE%=="" goto _calljava
+if ERRORLEVEL == 0 SET JAVAEXE=java
+if not "%JAVAEXE%"=="" goto _calljava
 
-if not %JAVA_HOME%=="" (SET JAVAEXE=%JAVA_HOME%\bin)
-if not %JAVAEXE%=="" goto _calljava
+if not %JAVA_HOME%=="" SET JAVAEXE=%JAVA_HOME%\bin
+if not "%JAVAEXE%"=="" goto _calljava
 
-if not %JRE_HOME%=="" (SET JAVAEXE=%JRE_HOME%\bin)
-if not %JAVAEXE%=="" goto _calljava
+if not %JRE_HOME%=="" SET JAVAEXE=%JRE_HOME%\bin
+if not "%JAVAEXE%"=="" goto _calljava
 
-if not %JDK_HOME%=="" (SET JAVAEXE=%JDK_HOME%\bin)
-if not %JAVAEXE%=="" goto _calljava
+if not %JDK_HOME%=="" SET JAVAEXE=%JDK_HOME%\bin
+if not "%JAVAEXE%"=="" goto _calljava
 
 goto _end
+
+REM =========================
 
 :_calljava
 "%JAVAEXE%" -cp "%~dp0\%JAVAJAR%" %JAVAOPTS% %MAINCLASS% %*
