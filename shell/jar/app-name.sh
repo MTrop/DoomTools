23a4dfa21a921c4bc324028eefce82eeea3b2805
mtrop <mtropiano@gmail.com>
2020-05-27 22:11:08 -0400

    Fixed up Bash scripts

--- shell/jar/app-name.sh
+++ shell/jar/app-name.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+JAVAOPTS={{JAVA_OPTIONS}}
+JAVAJAR={{JAR_NAME}}
+MAINCLASS={{MAIN_CLASSNAME}}
+
 CMD_READLINK="readlink -f"
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	function realpath {
@@ -9,8 +13,27 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 SCRIPTDIR="$(cd "$(dirname $($CMD_READLINK "$0"))"; pwd)"
 
-JAVAOPTS={{JAVA_OPTIONS}}
-JAVAJAR={{JAR_NAME}}
-MAINCLASS={{MAIN_CLASSNAME}}
+# Test for Java
+if [ -f "$SCRIPTDIR/jre/bin/java" ]; then
+	JAVACMD="$SCRIPTDIR/jre/bin/java"
+elif hash java 2>/dev/null; then
+	JAVACMD=java
+elif [ -n "${JAVA_HOME}" ]; then
+	JAVACMD="${JAVA_HOME}/bin/java"
+elif [ -n "${JDK_HOME}" ]; then
+	JAVACMD="${JDK_HOME}/bin/java"
+elif [ -n "${JRE_HOME}" ]; then
+	JAVACMD="${JRE_HOME}/java"
+fi
 
-java -cp "$SCRIPTDIR/$JAVAJAR" $JAVAOPTS $MAINCLASS $*
+if [[ -n "$JAVACMD" ]]; then
+	"$JAVACMD" -cp "$SCRIPTDIR/$JAVAJAR" $JAVAOPTS $MAINCLASS $*
+else
+	echo "Java 8 or higher could not be detected. To use these tools, a JRE must be"
+	echo "installed."
+	echo
+	echo "The enviroment variables JAVA_HOME, JRE_HOME, or JDK_HOME are not set to"
+	echo "your JRE or JDK directories, nor were Java binaries detected on your PATH."
+	echo
+	echo "For help, visit https://www.java.com/."
+fi
