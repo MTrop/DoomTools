0ceaed7e56683dbbed31a47fe4161c5881db6fa0
mtrop <mtropiano@gmail.com>
2021-09-21 00:45:00 -0400

    Altered ignore, included missing files

---
+++ src/main/resources/shell/embed/app-name.cmd
@@ -0,0 +1,61 @@
+@echo off
+
+REM ============================[ VARIABLES ]================================
+SETLOCAL
+SET JAVAEXE=
+SET JAVAOPTS={{JAVA_OPTIONS}}
+SET MAINCLASS={{MAIN_CLASSNAME}}
+SET DOOMTOOLS_PATH=%~dp0
+SET DOOMTOOLS_JAR={{JAR_NAME}}
+
+REM =========================================================================
+
+if not "%DOOMTOOLS_JAR%"=="" goto _findjava
+echo FATAL ERROR: DoomTools application JAR NOT FOUND!
+goto _end
+
+:_findjava
+REM ===== Java Scan
+
+if exist "%DOOMTOOLS_PATH%\jre\bin\java.exe" SET JAVAEXE=%DOOMTOOLS_PATH%\jre\bin\java.exe
+if not "%JAVAEXE%"=="" goto _calljava
+
+where java > nul
+if %ERRORLEVEL% == 0 SET JAVAEXE=java
+if not "%JAVAEXE%"=="" goto _calljava
+
+if not %JAVA_HOME%=="" SET JAVAEXE=%JAVA_HOME%\bin\java.exe
+if not "%JAVAEXE%"=="" goto _calljava
+
+if not %JDK_HOME%=="" SET JAVAEXE=%JDK_HOME%\bin\java.exe
+if not "%JAVAEXE%"=="" goto _calljava
+
+if not %JRE_HOME%=="" SET JAVAEXE=%JRE_HOME%\java.exe
+if not "%JAVAEXE%"=="" goto _calljava
+
+REM ===== No Java.
+
+echo Java 8 or higher could not be detected. To use these tools, a JRE must be 
+echo installed.
+echo.
+echo The environment variables JAVA_HOME, JRE_HOME, or JDK_HOME are not set to 
+echo your JRE or JDK directories, nor were Java binaries detected on your PATH.
+echo.
+echo For help, visit https://www.java.com/.
+echo.
+echo Java can be downloaded from the following places:
+echo.
+echo Azul:      https://www.azul.com/downloads/
+echo Microsoft: https://www.microsoft.com/openjdk
+echo Oracle:    https://java.com/en/download/
+echo.
+
+goto _end
+
+REM =========================
+
+:_calljava
+"%JAVAEXE%" -cp "%DOOMTOOLS_PATH%\%DOOMTOOLS_JAR%" %JAVAOPTS% %MAINCLASS% %*
+
+:_end
+ENDLOCAL
---
+++ src/main/resources/shell/embed/app-name.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+JAVAOPTS="{{JAVA_OPTIONS}}"
+MAINCLASS={{MAIN_CLASSNAME}}
+
+CMD_READLINK="readlink -f"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	function realpath {
+    	[ "." = "${1}" ] && n=${PWD} || n=${1}; while nn=$( readlink -n "$n" ); do n=$nn; done; echo "$n"
+	}
+	CMD_READLINK="realpath"
+fi
+DOOMTOOLS_PATH="$(cd "$(dirname $($CMD_READLINK "$0"))"; pwd)"
+DOOMTOOLS_JAR={{JAR_NAME}}
+
+# Test for Java
+if [ -f "${DOOMTOOLS_PATH}/jre/bin/java" ]; then
+	JAVACMD="${DOOMTOOLS_PATH}/jre/bin/java"
+elif hash java 2>/dev/null; then
+	JAVACMD=java
+elif [ -n "${JAVA_HOME}" ]; then
+	JAVACMD="${JAVA_HOME}/bin/java"
+elif [ -n "${JDK_HOME}" ]; then
+	JAVACMD="${JDK_HOME}/bin/java"
+elif [ -n "${JRE_HOME}" ]; then
+	JAVACMD="${JRE_HOME}/java"
+fi
+
+if [[ -n "$JAVACMD" ]]; then
+	"$JAVACMD" -cp "${DOOMTOOLS_PATH}/${DOOMTOOLS_JAR}" $JAVAOPTS $MAINCLASS $*
+else
+	echo "Java 8 or higher could not be detected. To use these tools, a JRE must be"
+	echo "installed."
+	echo
+	echo "The environment variables JAVA_HOME, JRE_HOME, or JDK_HOME are not set to"
+	echo "your JRE or JDK directories, nor were Java binaries detected on your PATH."
+	echo
+	echo "For help, visit https://www.java.com/."
+	echo "Java can be downloaded from the following places:"
+	echo
+	echo "Azul:      https://www.azul.com/downloads/"
+	echo "Microsoft: https://www.microsoft.com/openjdk"
+	echo "Oracle:    https://java.com/en/download/"
+	echo
+fi
