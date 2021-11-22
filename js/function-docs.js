a5bb82ec4b388ef6cbca90cdc53bf24b45632cba
mtrop <mtropiano@gmail.com>
2021-11-22 01:57:55 -0500

    change docs behavior

--- js/function-docs.js
+++ js/function-docs.js
@@ -1,8 +1,4 @@
 $DJMain(() => {
-	$DJ.class('category-title').click(function(){
-		const code = $DJ(this).attr('section-code');
-		$DJ.id('category-content-' + code).classToggle('show-section');
-	});
 	$DJ.class('function-title').click(function(){
 		const code = $DJ(this).attr('section-code');
 		$DJ.id('function-content-' + code).classToggle('show-section');
