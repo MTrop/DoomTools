2cbc54a4dcc5adb0ba9400b168e9c067e4c10cb7
mtrop <mtropiano@gmail.com>
2025-07-19 16:19:47 -0400

    Decouple re-tabber from static libs

--- src/test/java/net/mtrop/doom/tools/ReTabberMain.java
+++ src/test/java/net/mtrop/doom/tools/ReTabberMain.java
@@ -3,18 +3,18 @@ package net.mtrop.doom.tools;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.StringReader;
-
-import net.mtrop.doom.tools.struct.util.FileUtils;
-import net.mtrop.doom.tools.struct.util.IOUtils;
-import net.mtrop.doom.tools.struct.util.OSUtils;
+import java.nio.charset.Charset;
 
 public final class ReTabberMain
 {
-	public static final String NEWLINE = OSUtils.isWindows() ? "\r\n" : "\n";
+	public static final String NEWLINE = System.getProperty("os.name").contains("Windows") ? "\r\n" : "\n";
 	public static final int SPACE_AMOUNT = 4;
 	
 	public static void main(String[] args)
@@ -24,7 +24,7 @@ public final class ReTabberMain
 	
 	private static void respaceDirectory(File directory)
 	{
-		for (File javaFile : directory.listFiles((f) -> f.isDirectory() || FileUtils.getFileExtension(f).equalsIgnoreCase("java")))
+		for (File javaFile : directory.listFiles((f) -> f.isDirectory() || getFileExtension(f.getName(), ".").equalsIgnoreCase("java")))
 		{
 			if (javaFile.isDirectory())
 			{
@@ -42,7 +42,7 @@ public final class ReTabberMain
 	{
 		StringBuilder sb = new StringBuilder();
 		
-		try (BufferedReader br = IOUtils.openTextFile(f))
+		try (BufferedReader br = openTextFile(f))
 		{
 			String line;
 			while ((line = br.readLine()) != null)
@@ -118,4 +118,45 @@ public final class ReTabberMain
 		}
 	}
 	
+	/**
+	 * Returns the extension of a filename.
+	 * @param filename the file name.
+	 * @param extensionSeparator the text or characters that separates file name from extension.
+	 * @return the file's extension, or an empty string for no extension.
+	 */
+	public static String getFileExtension(String filename, String extensionSeparator)
+	{
+		int extindex = filename.lastIndexOf(extensionSeparator);
+		if (extindex >= 0)
+			return filename.substring(extindex+1);
+		return "";
+	}
+
+	/**
+	 * Convenience method for
+	 * <code>new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.defaultCharset()))</code>
+	 * @param file the file to open.
+	 * @return an open buffered reader for the provided file.
+	 * @throws IOException if an error occurred opening the file for reading.
+	 * @throws SecurityException if you do not have permission for opening the file.
+	 */
+	public static BufferedReader openTextFile(File file) throws IOException
+	{
+		return openTextStream(new FileInputStream(file), Charset.defaultCharset());
+	}
+
+	/**
+	 * Convenience method for
+	 * <code>new BufferedReader(new InputStreamReader(in, encoding))</code>
+	 * @param in the stream to read.
+	 * @param encoding the text encoding to use.
+	 * @return an open buffered reader for the provided stream.
+	 * @throws IOException if an error occurred opening the stream for reading.
+	 * @throws SecurityException if you do not have permission for opening the stream.
+	 */
+	public static BufferedReader openTextStream(InputStream in, Charset encoding) throws IOException
+	{
+		return new BufferedReader(new InputStreamReader(in, encoding));
+	}
+
 }
